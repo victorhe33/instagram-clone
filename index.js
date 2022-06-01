630d5bd6de48c4a25718e87e972f411fadc4e40d
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-01 15:05:34 -0400

    ADD fake password functionality to redirect

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 // CODE HERE
-console.log('hello');
+const accounts = {
+  admin: "password",
+};
+
+//SELECTORS/ELEMENTS
+const login = document.getElementById("login");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const forgotPassword = document.getElementById("forgotPassword");
+
+//FUNCTIONS
+function checkInput() {
+  let inputUsername = username.value;
+  let inputPassword = password.value;
+
+  if (accounts[inputUsername] === inputPassword) {
+    alert("your in!");
+    window.location.replace("feed.html");
+  } else {
+    alert("wrong pass bro");
+  }
+}
+
+//accomodate pressing enter;
+function checkKeypress(e) {
+  if (e.key === 'Enter') {
+    checkInput();
+  }
+}
+
+//EVENT LISTENERS
+login.addEventListener("click", checkInput);
+forgotPassword.addEventListener("click", () => alert('username: admin' + '\n' + 'password: "password"'));
+password.addEventListener("keypress", checkKeypress);
+username.addEventListener("keypress", checkKeypress);
+
