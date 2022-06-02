29f8f60e06b95b44e3de4c24b735585dd980db1f
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-02 19:10:06 -0400

    BUGGY commit, testing fetch img on remote

--- feed.js
+++ feed.js
@@ -1,35 +1,45 @@
 // CODE HERE
 const fetchPromise = fetch("https://us-central1-codesmith-curriculum-server.cloudfunctions.net/app/images");
-// console.log(fetchPromise);
 
-//storage array for all our image URLS
-// let urlArray = [];
+//when promise comes back, parse with json(), check if each url is valid, and then generate images on site.
+// fetchPromise.then(response => {
+//   return response.json();
+// }).then(images => {
+//   images.forEach(url => generate(url));
+// });
 
-//Fetch via promise from website, push all urls fetched into URL array.
 fetchPromise.then(response => {
   return response.json();
 }).then(images => {
-  const URL = images.forEach(url => generate(url));
+  images.forEach(url => {
+    fetch(url, { method: 'HEAD', credentials: "same-origin", mode: "no-cors"})
+      .then(res => {
+        if (res.ok) {
+          console.log('Image exists.');
+          generate(url);
+        } else {
+          console.log('Image does not exist.');
+        }
+      }).catch(err => console.log('Error:', err));
+  });
 });
 
-// console.log(urlArray);
+
+// fetch('https://via.placeholder.com/150', { method: 'GET', credentials: "same-origin" })
+//   .then(res => {
+//     if (res.ok) {
+//       console.log('Image exists.');
+//     } else {
+//       console.log('Image does not exist.');
+//     }
+//   }).catch(err => console.log('Error:', err));
 
 //QUERY SELECTOR /ID
 const imageContainer = document.getElementById("imageContainer");
 
-
 function generate(url) {  
-  // console.log("hey");
   const img = document.createElement("img");
   img.setAttribute('src', `${url}`);
+  img.setAttribute('alt', "image");
   imageContainer.appendChild(img);
 }
-
-// console.log(imageContainer);
-
-// const newDiv = document.createElement("div")
-// const heya = const div = document.createElement("div");
-// newDiv.appendChild(heya)
-// imageContainer.appendChild("hey")
-
-// imageContainer.textContent = "yoyoyo";
