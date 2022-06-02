fd32cf2d54b3d0ae8419029e7a6fb6159e0d5cd8
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-02 17:03:34 -0400

    INIT fetch img function implemented

--- feed.js
+++ feed.js
@@ -1,1 +1,35 @@
 // CODE HERE
+const fetchPromise = fetch("https://us-central1-codesmith-curriculum-server.cloudfunctions.net/app/images");
+// console.log(fetchPromise);
+
+//storage array for all our image URLS
+// let urlArray = [];
+
+//Fetch via promise from website, push all urls fetched into URL array.
+fetchPromise.then(response => {
+  return response.json();
+}).then(images => {
+  const URL = images.forEach(url => generate(url));
+});
+
+// console.log(urlArray);
+
+//QUERY SELECTOR /ID
+const imageContainer = document.getElementById("imageContainer");
+
+
+function generate(url) {  
+  // console.log("hey");
+  const img = document.createElement("img");
+  img.setAttribute('src', `${url}`);
+  imageContainer.appendChild(img);
+}
+
+// console.log(imageContainer);
+
+// const newDiv = document.createElement("div")
+// const heya = const div = document.createElement("div");
+// newDiv.appendChild(heya)
+// imageContainer.appendChild("hey")
+
+// imageContainer.textContent = "yoyoyo";
