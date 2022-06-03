7e22c29ab484a59e630f89244d0be29ed02a0e46
Victor <94798862+victorhe33@users.noreply.github.com>
2022-06-02 22:38:58 -0400

    FIX added onerror fx hide image if url is broken

--- feed.js
+++ feed.js
@@ -2,44 +2,30 @@
 const fetchPromise = fetch("https://us-central1-codesmith-curriculum-server.cloudfunctions.net/app/images");
 
 //when promise comes back, parse with json(), check if each url is valid, and then generate images on site.
-// fetchPromise.then(response => {
-//   return response.json();
-// }).then(images => {
-//   images.forEach(url => generate(url));
-// });
-
 fetchPromise.then(response => {
   return response.json();
 }).then(images => {
-  images.forEach(url => {
-    fetch(url, { method: 'HEAD', credentials: "same-origin", mode: "no-cors"})
-      .then(res => {
-        if (res.ok) {
-          console.log('Image exists.');
-          generate(url);
-        } else {
-          console.log('Image does not exist.');
-        }
-      }).catch(err => console.log('Error:', err));
-  });
+  images.forEach(url => generate(url));
 });
-
-
-// fetch('https://via.placeholder.com/150', { method: 'GET', credentials: "same-origin" })
-//   .then(res => {
-//     if (res.ok) {
-//       console.log('Image exists.');
-//     } else {
-//       console.log('Image does not exist.');
-//     }
-//   }).catch(err => console.log('Error:', err));
 
 //QUERY SELECTOR /ID
 const imageContainer = document.getElementById("imageContainer");
 
+//set a unique ID which will be incremented to give all containers unique IDs
+let uniqueId = 1;
+
+//create image/container with unique ID. if image doesnt load, onerror will call a function to hide container.
 function generate(url) {  
   const img = document.createElement("img");
   img.setAttribute('src', `${url}`);
   img.setAttribute('alt', "image");
+  img.setAttribute('id', `${uniqueId}`);
+  img.setAttribute('onerror', `removeImage(${uniqueId})`);
   imageContainer.appendChild(img);
+  uniqueId++;
+}
+
+function removeImage(identifier) {
+  const targetElement = document.getElementById(`${identifier}`);
+  targetElement.style.display = 'none';
 }
